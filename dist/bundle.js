403b74ed9c671d74e2c32cfda311f3da0640d2f6
Ryan <alejandr257@gmail.com>
2018-06-21 10:35:46 -0700

    Update particle colors and tail length

--- dist/bundle.js
+++ dist/bundle.js
@@ -147,8 +147,9 @@ class Game {
   }
 
   populateParticles() {
-    let colorArr = ['#ffaa33', '#99ffaaa', '#00ff00', '#4411aa', '#ff1100'];
-    for (var i = 0; i < 400; i++) {
+    // let colorArr = ['#ffaa33', '#99ffaaa', '#00ff00', '#4411aa', '#ff1100'];
+    let colorArr = ['#cc0099', '#0033cc', '#ffff66', '#1affff','#ff0000'];
+    for (var i = 0; i < 200; i++) {
       let x = Math.random() * this.ctx.canvas.width;
       let y = Math.random() * this.ctx.canvas.height;
       let dx = (Math.random() - 0.5) * 4;
@@ -327,7 +328,7 @@ class Particle {
     if (dist > gball.radius ){
       this.x += ((gball.radius / 5) * (dx / dist));
       this.y += ((gball.radius / 5) * (dy / dist));
-    } else if (dist <= gball.radius ) {
+    } else if (dist <= gball.radius + (2 * this.radius)) {
       this.x = (Math.random() - 0.5) * (2 * this.ctx.canvas.width);
       this.y = (Math.random() - 0.5) * (2 * this.ctx.canvas.height);
     }
@@ -345,7 +346,7 @@ class Particle {
   update() {
     this.draw();
 
-    this.history.splice(3, this.history.length);
+    this.history.splice(1, this.history.length);
     this.history.unshift(new Particle(this.ctx, this.x, this.y, this.radius - (this.radius/2),
     this.dx,this.dy, this.color));
 
