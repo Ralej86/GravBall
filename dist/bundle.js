9ce4965457fcb6888c143891be7aafabc2217b66
Ryan <alejandr257@gmail.com>
2018-06-21 00:00:28 -0700

    Implement many colors on particles

--- dist/bundle.js
+++ dist/bundle.js
@@ -88,7 +88,7 @@ __webpack_require__.r(__webpack_exports__);
 document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.getElementById("canvas")
   canvas.height = 600;
-  canvas.width = 900;
+  canvas.width = window.innerWidth;
 
   let ctx = canvas.getContext("2d");
   let mouse = {
@@ -106,6 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
     timer = setTimeout(() => {
       if (!stop) {
         game.populateGravityBall(event.x, event.y)
+        // game.populateGravityBall(event.x + 370, event.y + 175.5)
       }
       stop = false;
     }, delay);
@@ -120,6 +121,7 @@ document.addEventListener("DOMContentLoaded", () => {
     clearTimeout(timer);
     stop = true;
     game.removeGravityBall(event.x, event.y)
+    // game.removeGravityBall(event.x + 370, event.y + 175.5)
 
     // console.log("double-click");
     // console.log(mouse);
@@ -157,12 +159,14 @@ class Game {
   }
 
   populateParticles() {
+    let colorArr = ['#ffaa33', '#99ffaaa', '#00ff00', '#4411aa', '#ff1100'];
     for (var i = 0; i < 100; i++) {
       let x = Math.random() * this.ctx.canvas.width;
       let y = Math.random() * this.ctx.canvas.height;
       let dx = (Math.random() - 0.5) * 4;
       let dy = (Math.random() - 0.5) * 4
-      this.particles.push(new _particle_js__WEBPACK_IMPORTED_MODULE_0__["default"](this.ctx, x, y, 3, dx, dy))
+      let color = colorArr[Math.floor(Math.random() * colorArr.length)]
+      this.particles.push(new _particle_js__WEBPACK_IMPORTED_MODULE_0__["default"](this.ctx, x, y, 3, dx, dy, color))
     }
   }
 
@@ -267,13 +271,15 @@ class GravityBall{
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 class Particle {
-  constructor(ctx, x, y, radius, dx, dy) {
+  constructor(ctx, x, y, radius, dx, dy, color) {
     this.ctx = ctx;
     this.x = this.oldX = x;
     this.y = this.oldY = y;
     this.radius = radius;
     this.dx = dx;
     this.dy = dy;
+    this.color = color;
+    this.history = [];
 
     this.update = this.update.bind(this);
     this.animate = this.animate.bind(this);
@@ -282,9 +288,9 @@ class Particle {
   draw() {
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    this.ctx.fillStyle = "white";
+    this.ctx.fillStyle = this.color;
     this.ctx.fill();
-    this.ctx.strokeStyle = "white";
+    this.ctx.strokeStyle = this.ctx.fillStyle;
     this.ctx.stroke();
   }
 
@@ -296,9 +302,9 @@ class Particle {
     if (dist > 10 ){
       this.x += 3 * (dx / dist);
       this.y += 3 * (dy / dist);
-    } else if (dist <= 10 ) {
-      this.x = (Math.random() - 0.5) * (4 * this.ctx.canvas.width);
-      this.y = (Math.random() - 0.5) * (4 * this.ctx.canvas.height);
+    } else if (dist <= 15 ) {
+      this.x = (Math.random() - 0.5) * (2 * this.ctx.canvas.width);
+      this.y = (Math.random() - 0.5) * (2 * this.ctx.canvas.height);
     }
   }
 
@@ -313,14 +319,17 @@ class Particle {
 
   update() {
     this.draw();
+    // debugger
 
-    // if (this.x + this.radius > this.ctx.canvas.width || this.x - this.radius < 0) {
-    //   this.dx = -this.dx;
-    // }
-    //
-    // if (this.y + this.radius > this.ctx.canvas.height || this.y - this.radius < 0) {
-    //   this.dy = -this.dy;
-    // }
+    this.history.splice(3, this.history.length);
+    this.history.unshift(new Particle(this.ctx, this.x, this.y, this.radius - (this.radius/2),
+    this.dx,this.dy, this.color));
+
+    for (var i = 0; i < this.history.length; i++) {
+      // debugger
+      this.history[i].draw()
+    }
+    // debugger
 
     this.x += this.dx;
     this.y += this.dy;
@@ -328,7 +337,6 @@ class Particle {
 
   animate() {
     this.update();
-
   }
 }
 
